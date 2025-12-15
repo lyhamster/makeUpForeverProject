bcbce61eb549ab1f6cb15e3185c1dfc68c4802ac
lyly dev <lyanne.n619@gmail.com>
2025-12-15 13:44:43 +0100

    feat: add intersection observation + add landing anim on discover section

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 import "./main.scss"
 import "./animation.scss"
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add("inview");
+            return;
+        }
+    });
+}, {
+    threshold: 0.4
+});
+
+const landingAnimTest = document.querySelectorAll(".landingAnim");
+landingAnimTest.forEach(div => observer.observe(div)) ;
+
+
